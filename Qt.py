a937b2b9f3f5f733e4b6a6d97532663b97f4cdeb
Anton Basov <anton.iv90@mail.ru>
2022-06-30 09:41:24 +0300

    Сделана форма.

--- Qt.py
+++ Qt.py
@@ -2,8 +2,8 @@ import PySide2
 from PySide2 import QtWidgets, QtCore, QtGui
 
 from main import Game
-from area import Ui_Form
-from help import Ui_Help
+from area2 import Ui_Form
+#from help import Ui_Help
 x = Game()
 
 
@@ -13,7 +13,7 @@ class MirrorWindow(QtWidgets.QWidget):
         self.ui = Ui_Form()
         self.ui.setupUi(self)
 
-        self.ui.pushButton_help.clicked.connect(self.help)
+        #self.ui.pushButton_help.clicked.connect(self.help)
         self.ui.pushButton_start.clicked.connect(self.start_new_game)
         self.ui.pushButton_up.clicked.connect(self.turn_player)
         self.ui.pushButton_down.clicked.connect(self.turn_player)
@@ -22,7 +22,8 @@ class MirrorWindow(QtWidgets.QWidget):
 
     @QtCore.Slot()
     def display_output(self):
-        color_le = {2: 'background-color: LemonChiffon',
+        color_le = {0: 'background-color: red',
+                    2: 'background-color: LemonChiffon',
                     4: 'background-color: PapayaWhip',
                     8: 'background-color: PeachPuff',
                     16: 'background-color: LightSalmon',
@@ -50,32 +51,32 @@ class MirrorWindow(QtWidgets.QWidget):
 
     @QtCore.Slot()
     def turn_player(self):
-        tern = {"UP": "w",
-                "DOWN": "s",
-                "LEFT": "a",
-                "RIGHT": "d"}
+        tern = {"Вверх": "w",
+                "Вниз": "s",
+                "Влево": "a",
+                "Вправо": "d"}
         x.input_play(tern.get(self.sender().text()))
         self.display_output()
         x.add_two()
         self.display_output()
         self.ui.lcdNumber.display(x.show_score())
 
-    @QtCore.Slot()
-    def help(self):
-        self.help = HelpApp()
-        self.help.show()
+    # @QtCore.Slot()
+    # def help(self):
+    #     self.help = HelpApp()
+    #     self.help.show()
 
-    @QtCore.Slot()
-    def closeEvent(self, event: QtGui.QCloseEvent) -> None:
-        self.help.close()
-        event.accept()
+    # @QtCore.Slot()
+    # def closeEvent(self, event: QtGui.QCloseEvent) -> None:
+    #     self.help.close()
+    #     event.accept()
 
 
-class HelpApp(QtWidgets.QWidget):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.ui = Ui_Help()
-        self.ui.setupUi(self)
+# class HelpApp(QtWidgets.QWidget):
+#     def __init__(self, parent=None):
+#         super().__init__(parent)
+#         self.ui = Ui_Help()
+#         self.ui.setupUi(self)
 
 
 if __name__ == '__main__':
